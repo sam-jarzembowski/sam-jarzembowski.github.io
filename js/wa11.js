ae6b5b1741c8551834231f96ba428a3aa3c8aaf2
sam-jarzembowski <samuel.jarzembowski@colorado.edu>
2023-11-09 14:41:52 -0700

    wa11 work

--- js/wa11.js
+++ js/wa11.js
@@ -5,22 +5,34 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imgArray = ['../img/wa11/pic1.jpg', '../img/wa11/pic2.jpg', '../img/wa11/pic3.jpg', '../img/wa11/pic4.jpg', '../img/wa11/pic5.jpg'];
+const imgArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
 var altDict = {
-    '../img/wa11/pic1.jpg' : "alt text",
-    '../img/wa11/pic2.jpg' : "alt text",
-    '../img/wa11/pic3.jpg' : "alt text",
-    '../img/wa11/pic4.jpg' : "alt text",
-    '../img/wa11/pic5.jpg' : "alt text",
+    'pic1.jpg' : "alt text",
+    'pic2.jpg' : "alt text",
+    'pic3.jpg' : "alt text",
+    'pic4.jpg' : "alt text",
+    'pic5.jpg' : "alt text",
 }
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (imgage of imgArray) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `../img/wa11/${image}`);
+    newImage.setAttribute('alt', altDict[image]);
+    thumbBar.appendChild(newImage);
+}
 
 /* Wiring up the Darken/Lighten button */
+if (btn.getAttribute('class') === dark ) {
+    btn.setAttribute("class", "light");
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+}
+
+else {
+    btn.setAttribute("class", "dark");
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgba(0,0,0,0)";
+}
