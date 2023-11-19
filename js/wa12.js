75c582a1443d306afa960bcacffd5fce968afa55
sam-jarzembowski <samuel.jarzembowski@colorado.edu>
2023-11-19 13:53:40 -0800

    wa12 work

--- js/wa12.js
+++ js/wa12.js
@@ -1,15 +1,17 @@
 const refreshBtn = document.getElementById('#js-refresh');
 refreshBtn.addEventListener('click', refresh);
-const dogImage = document.getElementById('#js-dog-image');
+const catImage = document.getElementById('#js-cat-image');
+const factText = document.getElementById('#js-fact');
 
-const img_url = 'https://dog.ceo/api/breeds/image/random';
+const img_url = 'https://api.thecatapi.com/v1/images/search';
 
 async function getImage() {
     try {
         const response = await fetch(img_url);
         const json = await response.json();
-        let link = json.message;
-        dogImage.src = link;
+        console.log(json);
+        let link = json["url"];
+        catImage.src = link;
         
     }
     catch(err) {
@@ -19,14 +21,25 @@ async function getImage() {
 
 }
 
-const fact_url = '';
+const fact_url = 'https://meowfacts.herokuapp.com/';
 
 async function getFact() {
-
+    try {
+        const response = await fetch(fact_url);
+        const json = await response.json();
+        console.log(json);
+        let fact = json["data"];
+        factText.textContent = fact;
+    }
+    catch(err) {
+        console.log(err);
+        alert('Failed to retrieve fact')
+    }
 }
 
 async function refresh() {
     getImage();
+    getFact();
 }
 
 refresh();
