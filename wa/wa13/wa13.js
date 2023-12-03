91d0b66f5aeb7b8399cd6206dbdefac67539a62e
sam-jarzembowski <samuel.jarzembowski@colorado.edu>
2023-12-03 13:28:19 -0700

    wa13

--- wa/wa13/wa13.js
+++ wa/wa13/wa13.js
@@ -21,18 +21,11 @@ const employeeInfo = {
             "Designation" : "Executive",
             "Salary" : "21200",
             "Raise Eligible" : "false"
-        },
-        {
-            "Name" : "Anna",
-            "Department" : "Tech",
-            "Designation" : "Executive",
-            "Salary" : "25600",
-            "Raise Eligible" : "false"
         }
     ]
 };
 
-console.log("Question 1:");
+console.log("*** Question 1 ***");
 console.log(employeeInfo);
 
 // Q2
@@ -46,19 +39,49 @@ const companyInfo = {
     ]
 };
 
-console.log("Question 2");
+console.log("*** Question 2 ***");
 console.log(companyInfo);
 
 // Q3
-// see above
-console.log("Question 3");
+employeeInfo.employees.push(
+    {
+        "Name" : "Anna",
+        "Department" : "Tech",
+        "Designation" : "Executive",
+        "Salary" : "25600",
+        "Raise Eligible" : "false"
+    }
+);
+console.log("*** Question 3 ***");
 console.log(employeeInfo);
 
 // Q4
-const totalSalary = 0;
-for (employees in employeeInfo) {
-    totalSalary += int(employeeInfo[0]["Salary"]);
+let totalSalary = 0;
+for (employee in employeeInfo.employees) {
+    totalSalary += parseInt(employee.Salary);
 }
-console.log(totalSalary);
+console.log("*** Question 4 ***");
+console.log('Total salary: $',totalSalary);
 
 // Q5
+for (employee in employeeInfo.employees) {
+    if (employee["Raise Eligible"] === "true") {
+        employee.Salary = employee.Salary * 1.1;
+        employee["Raise Eligible"] = "false";
+    }
+}
+console.log("*** Question 5 ***");
+console.log(employeeInfo);
+
+// Q6
+const wfh_employees = ["Anna", "Sam"];
+for (employee in employeeInfo.employees) {
+    if (wfh_employees.includes(employee["Name"])) {
+        employee.wfh = "true";
+    }
+    else {
+        employee.wfh = "false";
+    }
+}
+console.log("*** Question 6 ***");
+console.log(employeeInfo);
